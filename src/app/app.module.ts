7171e2297125d36efa6e1bd5aec0ebf2b572ed34
Fendorr <JeaSchneider@web.de>
2022-01-28 17:44:46 +0100

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 import { HomeComponent } from './home/home.component';
 import { SpaceholderComponent } from './spaceholder/spaceholder.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
 import { MaterialModule } from './material.module';
 
 
@@ -17,9 +17,9 @@ import { MaterialModule } from './material.module';
   declarations: [
     AppComponent,
     TopBarComponent,
+    SidenavComponent,
     HomeComponent,
     SpaceholderComponent,
-    SidenavComponent
   ],
   imports: [
     BrowserModule,
