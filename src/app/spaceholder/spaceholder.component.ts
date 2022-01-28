aa19fffbea781370a155879cb95d3d35ff5be4e2
Fendorr <JeaSchneider@web.de>
2022-01-28 17:12:49 +0100

    top-bar + sidenav implemented

---
+++ src/app/spaceholder/spaceholder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spaceholder',
+  templateUrl: './spaceholder.component.html',
+  styleUrls: ['./spaceholder.component.css']
+})
+export class SpaceholderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
