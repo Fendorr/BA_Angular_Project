0df5ecc4138522abfd338002ce039549b7d4bd67
Fendorr <JeaSchneider@web.de>
2022-06-04 11:02:38 +0200

    jpgs converted to webp

--- src/app/GridExample/GridExample.component.ts
+++ src/app/GridExample/GridExample.component.ts
@@ -7,14 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GridExampleComponent implements OnInit {
   //#region imgs
-  doggo1: string = "assets/img/doggo1.jpg"
-  doggo2: string = "assets/img/doggo2.jpg"
+  doggo1: string = "assets/img/doggo1.webp"
+  doggo2: string = "assets/img/doggo2.webp"
   doggo3: string = "assets/img/doggo3.webp"
-  doggo4: string = "assets/img/doggo4.jpg"
-  doggo5: string = "assets/img/doggo5.jpeg"
-  robbenbaby1: string = "assets/img/robbenbaby1.jpg"
-  robbenbaby2: string = "assets/img/robbenbaby2.jpg"
-  robbenbaby4: string = "assets/img/robbenbaby4.jpg"
+  doggo4: string = "assets/img/doggo4.webp"
+  doggo5: string = "assets/img/doggo5.webp"
+  robbenbaby1: string = "assets/img/robbenbaby1.webp"
+  robbenbaby2: string = "assets/img/robbenbaby2.webp"
+  robbenbaby4: string = "assets/img/robbenbaby4.webp"
   //#endregion
   
   text: string = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, \
